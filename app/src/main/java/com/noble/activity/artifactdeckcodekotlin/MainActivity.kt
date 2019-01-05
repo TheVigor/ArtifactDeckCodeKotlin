b3af7dfebd833b16197bb0fd215b9d1a45fe4ceb
TheVigor <igor.proskuryakov14@gmail.com>
2019-01-05 17:45:21 +0300

    added simple test

--- app/src/main/java/com/noble/activity/artifactdeckcodekotlin/MainActivity.kt
+++ app/src/main/java/com/noble/activity/artifactdeckcodekotlin/MainActivity.kt
@@ -5,23 +5,36 @@ import android.os.Bundle
 import android.widget.Toast
 import com.noble.activity.artifactdeckcodekotlin.coder.ArtifactDeckDecoder
 import com.noble.activity.artifactdeckcodekotlin.coder.ArtifactDeckEncoder
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val firstDeckCode = "ADCJQUQI30zuwEYg2ABeF1Bu94BmWIBTEkLtAKlAZakAYmHh0JsdWUvUmVkIEV4YW1wbGU_"
+        private const val secondDeckCode = "ADCJWkTZX05uwGDCRV4XQGy3QGLmqUBg4GQJgGLGgO7AaABR3JlZW4vQmxhY2sgRXhhbXBsZQ__"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val encoder: ArtifactDeckEncoder = ArtifactDeckEncoder()
-        val decoder: ArtifactDeckDecoder = ArtifactDeckDecoder()
+        val encoder = ArtifactDeckEncoder()
+        val decoder = ArtifactDeckDecoder()
 
-        //ADCJQUQI30zuwEYg2ABeF1Bu94BmWIBTEkLtAKlAZakAYmHh0JsdWUvUmVkIEV4YW1wbGU_
-        //ADCJWkTZX05uwGDCRV4XQGy3QGLmqUBg4GQJgGLGgO7AaABR3JlZW4vQmxhY2sgRXhhbXBsZQ__
+        val deck = decoder.parseDeck(firstDeckCode)
+        val deckCode = encoder.encodeDeck(deck)
 
-        val deck = decoder.parseDeck("ADCJWkTZX05uwGDCRV4XQGy3QGLmqUBg4GQJgGLGgO7AaABR3JlZW4vQmxhY2sgRXhhbXBsZQ__")
-        Toast.makeText(this, deck.name, Toast.LENGTH_LONG).show()
+        if (deckCode != firstDeckCode) {
+            throw Exception("First deck mismatch")
+        }
 
-        val str = encoder.encodeDeck(deck)
+        val deck2 = decoder.parseDeck(secondDeckCode)
+        val deckCode2 = encoder.encodeDeck(deck2)
 
+        if (deckCode2 != secondDeckCode) {
+            throw Exception("Second deck mismatch")
+        }
+
+        Toast.makeText(this, deck.name + " " + deck2.name, Toast.LENGTH_LONG).show()
     }
 }
